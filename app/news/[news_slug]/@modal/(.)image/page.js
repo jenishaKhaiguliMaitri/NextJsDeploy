79d1cf5d7140f1ccbea20474c7d544ac7915d782
Jenisha <jenisha@192.168.1.6>
2024-12-08 08:47:02 +0545

    19: navigating programatically (useRouter)

--- app/news/[news_slug]/@modal/(.)image/page.js
+++ app/news/[news_slug]/@modal/(.)image/page.js
@@ -1,8 +1,11 @@
+"use client";
 import { DUMMY_NEWS } from "@/dummy-news";
-import { notFound } from "next/navigation";
+import { notFound, useRouter } from "next/navigation";
 import React from "react";
 
 const InterceptedImagePage = ({ params }) => {
+  // we can also refresh the message
+  const router = useRouter();
   const newsItemSlug = params.news_slug;
   const newsItem = DUMMY_NEWS.find((news) => news.slug === newsItemSlug);
   if (!newsItem) {
@@ -10,7 +13,7 @@ const InterceptedImagePage = ({ params }) => {
   }
   return (
     <>
-      <div className="modal-backdrop" />
+      <div className="modal-backdrop" onClick={router.back} />
       <dialog className="modal" open>
         <div className="fullscreen-image">
           <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
