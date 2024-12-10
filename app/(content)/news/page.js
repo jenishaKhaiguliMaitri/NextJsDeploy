d5881012d87ec2d5860c39f565bd255b4e80c622
Jenisha <jenisha@192.168.1.4>
2024-12-10 18:11:16 +0545

    04: server side data fetching

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,11 +1,16 @@
 import NewsList from "@/component/news-list";
 import { DUMMY_NEWS } from "@/dummy-news";
 
-const News = () => {
+const News = async () => {
+  const response = await fetch("http://localhost:8080/news");
+  if (!response.ok) {
+    throw new Error("Failed to fetch news.");
+  }
+  const news = await response.json();
   return (
     <div>
       <h1>News</h1>
-      <NewsList newsList={DUMMY_NEWS} />
+      <NewsList newsList={news} />
     </div>
   );
 };
