1a285d97b9c8086fb388fd603ebe31ae27c6ecb4
Jenisha <jenisha@192.168.1.4>
2024-12-10 18:16:13 +0545

    5: why use a separate be Fetching directly from the source

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,12 +1,15 @@
 import NewsList from "@/component/news-list";
 import { DUMMY_NEWS } from "@/dummy-news";
+import { getAllNews } from "@/lib/news";
 
 const News = async () => {
-  const response = await fetch("http://localhost:8080/news");
-  if (!response.ok) {
-    throw new Error("Failed to fetch news.");
-  }
-  const news = await response.json();
+  // const response = await fetch("http://localhost:8080/news");
+  // if (!response.ok) {
+  //   throw new Error("Failed to fetch news.");
+  // }
+  // const news = await response.json();
+
+  const news = getAllNews();
   return (
     <div>
       <h1>News</h1>
